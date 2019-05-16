238dfa66da1e4e10d7e6cf7a2059a9c6d79591b9
a805429509 <a805429509@gmail.com>
2019-05-16 17:32:52 +0800

    不晓得什么鬼，不能刷新列表的数据...回家弄

--- lib/MyListView.dart
+++ lib/MyListView.dart
@@ -1,6 +1,7 @@
 import 'package:english_words/english_words.dart' as ew;
-import 'package:flutter/material.dart';
 import 'package:english_words/english_words.dart';
+import 'package:flutter/material.dart';
+
 
 class MyListView extends StatefulWidget {
   @override
@@ -10,22 +11,28 @@ class MyListView extends StatefulWidget {
 class _MyListViewState extends State<MyListView> {
   final List<WordPair> _suggestions = <WordPair>[];
 
+  Future<Null> _refresh() async {
+    this._suggestions.clear();
+    await this._suggestions.addAll(ew.generateWordPairs().take(20));
+    return;
+  }
+
   Widget _buildSuggestions() {
-    return new ListView.builder(
-      padding: const EdgeInsets.all(19.0),
-      itemBuilder: (BuildContext _context, int i) {
-        //如果渲染到了最后一个，就给新加进去5个单词组，从而实现了无限下拉
-        if (i >= _suggestions.length) {
-          print('开始请求新的wordpair,现在长度为' + _suggestions.length.toString());
-          _suggestions.addAll(ew.generateWordPairs().take(5));
-          print('完成后的长度' + _suggestions.length.toString());
-        }
-        return Container(
-          child: Column(
-            children: <Widget>[_buildRow(_suggestions[i], i), new Divider()],
-          ),
-        );
-      },
+    return Container(
+      child: Scaffold(
+        body: RefreshIndicator(
+          onRefresh: _refresh,
+          child: new ListView.builder(itemBuilder: (BuildContext context,int i){
+            if(i>=this._suggestions.length){
+              this._suggestions.addAll(ew.generateWordPairs().take(5));
+            }
+            return Container(child: Column(children: <Widget>[
+              _buildRow(this._suggestions[i], i),
+              new Divider(height: 2.0,),
+            ],),);
+          },),
+        ),
+      ),
     );
   }
 
