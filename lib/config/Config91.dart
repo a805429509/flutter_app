ea7186421209b4e503bf1232161f1e1abf9724be
a805429509 <a805429509@gmail.com>
2019-06-17 01:46:52 +0800

    发现了原因。。后续可能会考虑在视频页面页加入一个底线功能

--- lib/config/Config91.dart
+++ lib/config/Config91.dart
@@ -1,12 +1,12 @@
 class Config91 {
   static final videoCategories = {
+    '最近加精(首页)': 'http://91porn.com/v.php?category=rf&viewtype=basic&page=',
     '当前最热': 'http://91porn.com/v.php?category=hot&viewtype=basic&page=',
     '最近得分': 'http://91porn.com/v.php?category=rp&viewtype=basic&page=',
     '10分钟以上': 'http://91porn.com/v.php?category=long&viewtype=basic&page=',
     '本月讨论': 'http://91porn.com/v.php?category=md&viewtype=basic&page=',
     '本月收藏': 'http://91porn.com/v.php?category=tf&viewtype=basic&page=',
     '收藏最多': 'http://91porn.com/v.php?category=rp&viewtype=basic&page=',
-    '最近加精(首页)': 'http://91porn.com/v.php?category=rf&viewtype=basic&page=',
     '本月最热': 'http://91porn.com/v.php?category=top&viewtype=basic&page=',
     //这里的m参数是月数减1
     '上月最热': 'http://91porn.com/v.php?category=top&m=-1&viewtype=basic&page=',
